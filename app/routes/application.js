d2003473ee5e5c9a0df59b758452133ccc93cd38
allthesignals <wilburnforce@gmail.com>
2018-02-21 16:16:42 -0500

    Formatting

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,6 +3,8 @@ import fetch from 'fetch';
 
 export default Route.extend({
   model() {
-    return fetch('/assets/cms/content.json').then(blob => blob.json()).then(({ config }) => config);
+    return fetch('/assets/cms/content.json')
+      .then(blob => blob.json())
+      .then(({ config }) => config);
   },
 });
