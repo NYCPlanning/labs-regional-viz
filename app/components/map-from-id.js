3ea66e54cd6e511086f5d34ad6462544098433df
Andy Cochran <acochran@planning.nyc.gov>
2018-02-21 15:54:52 -0500

    full-height map when no narrative

--- app/components/map-from-id.js
+++ app/components/map-from-id.js
@@ -2,6 +2,7 @@ import Component from '@ember/component';
 import mapboxgl from 'mapbox-gl';
 
 export default Component.extend({
+  classNameBindings: ['noNarrative:no-narrative'],
   classNames: 'map-container cell large-auto',
   zoom: 8.5,
   center: [-73.869324, 40.815888],
