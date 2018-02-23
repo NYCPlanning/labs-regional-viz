433e748abaf19b6d6aaf373b0f8ddb19171d3fc9
allthesignals <wilburnforce@gmail.com>
2018-02-23 12:54:16 -0500

    Fix data structure for route; shows content now

--- app/routes/map.js
+++ app/routes/map.js
@@ -9,7 +9,11 @@ export default Route.extend({
   model({ slug }) {
     const { maps = [] } = this
       .modelFor('application');
-    const [mapConfig] = maps.findBy('slug', slug).map;
+    const mapNarrative = maps.findBy('slug', slug);
+
+    // find the first map config because the
+    // design doesn't support multiple
+    const mapConfig = mapNarrative.map;
     const { sources } = mapConfig;
 
     const cartoSourcePromises = Object.keys(sources || {})
@@ -30,7 +34,7 @@ export default Route.extend({
     return Promise.all(cartoSourcePromises)
       .then((cartoPromises) => {
         mapConfig.sources = cartoPromises;
-        return mapConfig;
+        return mapNarrative;
       });
   },
 });
