bf9f4dd3ae589b76c79dd3404f984fa60e558ccb
allthesignals <wilburnforce@gmail.com>
2018-02-23 15:44:16 -0500

    Reorganize YAMLs

--- app/routes/map.js
+++ app/routes/map.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 
+import { get } from '@ember/object';
 import Route from '@ember/routing/route';
 import carto from 'ember-jane-maps/utils/carto';
 
@@ -7,12 +8,10 @@ const { Promise } = Ember.RSVP;
 
 export default Route.extend({
   model({ slug }) {
-    const { maps = [] } = this
+    const { maps = {} } = this
       .modelFor('application');
-    const mapNarrative = maps.findBy('slug', slug);
+    const mapNarrative = get(maps, slug);
 
-    // find the first map config because the
-    // design doesn't support multiple
     const mapConfig = mapNarrative.map;
     const { sources } = mapConfig;
 
