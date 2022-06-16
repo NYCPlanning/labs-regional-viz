fb7d25b06a2a7c84604c49b3841f604c975c5d5a
dara <dgoldberg@planning.nyc.gov>
2022-06-16 15:22:17 -0400

    update_file_reference_v220615

--- app/utils/get-popup-sql.js
+++ app/utils/get-popup-sql.js
@@ -18,13 +18,13 @@ export default function getPopupSQL(lngLat = { lng: 0, lat: 0 }, mapConfig = { p
 
   SQLArray.push(`
     SELECT 'region' as geomtype, null as islitown, null as ishouperm, null as iscommap, null as iscomnycwork, null as iscomnycres, name as name ${getPopupValue('region')}
-    FROM region_region_v210924
+    FROM region_region_v220615
   `);
 
   if (getPopupValue('subregion')) {
     SQLArray.push(`
       SELECT 'subregion' as geomtype, null as islitown, null as ishouperm, null as iscommap, null as iscomwork, null as iscomres, name as name ${getPopupValue('subregion')}
-      FROM region_subregion_v210924
+      FROM region_subregion_v220615
       WHERE ST_Intersects(the_geom, ST_SetSRID(ST_Point(${lng}, ${lat}), 4326))
     `);
   }
@@ -34,7 +34,7 @@ export default function getPopupSQL(lngLat = { lng: 0, lat: 0 }, mapConfig = { p
   if (getPopupValue('county')) {
     SQLArray.push(`
       SELECT 'county' as geomtype, null as islitown, null as ishouperm, iscommap, iscomwork, iscomres, name as name ${getPopupValue('county')}
-      FROM region_county_v210924
+      FROM region_county_v220615
       WHERE ST_Intersects(the_geom, ST_SetSRID(ST_Point(${lng}, ${lat}), 4326))
     `);
   }
@@ -44,7 +44,7 @@ export default function getPopupSQL(lngLat = { lng: 0, lat: 0 }, mapConfig = { p
   if (getPopupValue('municipality')) {
     SQLArray.push(`
       SELECT 'municipality' as geomtype, islitown, ishouperm, null as iscommap, null as iscomwork, null as iscomres, namelsad as name ${getPopupValue('municipality')}
-      FROM region_municipality_v210924
+      FROM region_municipality_v220615
       WHERE ST_Intersects(the_geom, ST_SetSRID(ST_Point(${lng}, ${lat}), 4326))
     `);
   }
