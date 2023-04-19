41a33e7f069219607cbc68d0c9c569938f198a31
tangoyankee <miller.tim108@gmail.com>
2023-04-19 09:23:36 -0400

    v3.13.0...v3.14.0

--- app/router.js
+++ app/router.js
@@ -1,32 +1,30 @@
 import EmberRouter from '@ember/routing/router';
-import { get } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { scheduleOnce } from '@ember/runloop';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  metrics: service(),
+export default class Router extends EmberRouter {
+  @service metrics;
 
   didTransition(...args) {
     this._super(...args);
     this._trackPage();
-  },
+  }
 
   _trackPage() {
     scheduleOnce('afterRender', this, () => {
       const page = this.get('url');
       const title = this.getWithDefault('currentRouteName', 'unknown');
-      get(this, 'metrics').trackPage({ page, title });
+      this.metrics.trackPage({ page, title });
     });
-  },
+  }
 
-  location: config.locationType,
-  rootURL: config.rootURL,
-});
+  location = config.locationType;
+
+  rootURL = config.rootURL;
+}
 
 Router.map(function() { // eslint-disable-line
   this.route('page', { path: '/:slug' });
   this.route('map', { path: ':categorySlug/:slug' });
 });
-
-export default Router;
